67c03f94658ea28df26b4ff841f797fcd76691fb
dipdastoko <dipdastoko@gmail.com>
2022-04-12 03:23:23 +0600

    display in ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import './App.css';
 import initializeAuthentication from './Firebase/Firebase.initialize';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { useState } from 'react';
 
 initializeAuthentication();
 const provider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({});
+
   const handleButtonClick = () => {
     const auth = getAuth();
     signInWithPopup(auth, provider)
       .then(result => {
-        const user = result.user;
-        console.log(user);
+        const { displayName, email, photoURL } = result.user;
+        const logedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(logedInUser);
+      })
+      .catch(error => {
+        console.log(error.message);
       })
   }
   return (
     <div className="App">
       <button onClick={handleButtonClick}>Sign In With Google</button>
+      {
+        user.email && <div>
+          <h2>Welcome {user.name}</h2>
+          <p>I know your email address: {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
