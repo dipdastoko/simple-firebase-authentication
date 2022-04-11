c972cb79d9eb4710f2897a84ab6642a86d9b7cdc
dipdastoko <dipdastoko@gmail.com>
2022-04-11 18:56:37 +0600

    firebase initialized

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfcDE2qep4efFlqSdeXNremqGLQl5CXsI",
+    authDomain: "simple-firebase-authenti-a0ffb.firebaseapp.com",
+    projectId: "simple-firebase-authenti-a0ffb",
+    storageBucket: "simple-firebase-authenti-a0ffb.appspot.com",
+    messagingSenderId: "717408135755",
+    appId: "1:717408135755:web:98f4e127a77db0f2927bce"
+};
+
+export default firebaseConfig;
