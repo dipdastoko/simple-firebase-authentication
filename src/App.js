f34305334f24f62f7f3272344963648653c805fe
dipdastoko <dipdastoko@gmail.com>
2022-04-12 03:08:48 +0600

    google authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import initializeAuthentication from './Firebase/Firebase.initialize';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+initializeAuthentication();
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const handleButtonClick = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+  }
   return (
     <div className="App">
-
+      <button onClick={handleButtonClick}>Sign In With Google</button>
     </div>
   );
 }
