89c764fdfc6f48e150fffb69dd4cadd01162765b
dipdastoko <dipdastoko@gmail.com>
2022-04-12 04:03:31 +0600

    github authentication added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import initializeAuthentication from './Firebase/Firebase.initialize';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
 import { useState } from 'react';
 
 initializeAuthentication();
-const provider = new GoogleAuthProvider();
-
+const googleProvider = new GoogleAuthProvider();
+const gitProvider = new GithubAuthProvider();
+const auth = getAuth();
 function App() {
   const [user, setUser] = useState({});
 
-  const handleButtonClick = () => {
-    const auth = getAuth();
-    signInWithPopup(auth, provider)
+  const handleGoogleSignIn = () => {
+
+    signInWithPopup(auth, googleProvider)
       .then(result => {
         const { displayName, email, photoURL } = result.user;
         const logedInUser = {
@@ -25,11 +26,26 @@ function App() {
         console.log(error.message);
       })
   }
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, gitProvider)
+      .then(result => {
+        const { displayName, photoURL, email } = result.user;
+        console.log(result.user);
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(loggedInUser);
+      })
+  }
   return (
     <div className="App">
-      <button onClick={handleButtonClick}>Sign In With Google</button>
+      <button onClick={handleGoogleSignIn}>Sign In With Google</button>
+      <button onClick={handleGithubSignIn}>Sign In With Github</button>
       {
-        user.email && <div>
+
+        user.name && <div>
           <h2>Welcome {user.name}</h2>
           <p>I know your email address: {user.email}</p>
           <img src={user.photo} alt="" />
